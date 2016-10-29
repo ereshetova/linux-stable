01890e95b6bc4687b4eacd122a00f8064e04c4fe
David Windsor <dwindsor@gmail.com>
2016-10-29 08:01:27 -0400

    tty: add overflow protection to struct tty_ldisc_ops.refcount
    Change type of struct tty_ldisc_ops.refcount to atomic_t.  This enables
    overflow protection: when CONFIG_HARDENED_ATOMIC is enabled, atomic_t
    variables cannot be overflowed.
    
    The copyright for the original PAX_REFCOUNT code:
      - all REFCOUNT code in general: PaX Team <pageexec@freemail.hu>
      - various false positive fixes: Mathias Krause <minipli@googlemail.com>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -2465,7 +2465,8 @@ void n_tty_inherit_ops(struct tty_ldisc_ops *ops)
 {
 	*ops = n_tty_ops;
 	ops->owner = NULL;
-	ops->refcount = ops->flags = 0;
+	atomic_set(&ops->refcount, 0);
+	ops->flags = 0;
 }
 EXPORT_SYMBOL_GPL(n_tty_inherit_ops);
 
